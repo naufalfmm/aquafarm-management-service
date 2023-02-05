62a41bab8c33272e17e19668abec916a14e07570
Naufal Farras <muhammadnaufalfm@gmail.com>
2023-02-05 22:58:03 +0700

    [MODIFY] response status code

--- infrastructures/rest/controllers/farms/deleteById.go
+++ infrastructures/rest/controllers/farms/deleteById.go
@@ -8,6 +8,7 @@ import (
 	"github.com/naufalfmm/aquafarm-management-service/consts"
 	"github.com/naufalfmm/aquafarm-management-service/utils/generateResp"
 	"github.com/naufalfmm/aquafarm-management-service/utils/token"
+	"gorm.io/gorm"
 )
 
 func (c Controllers) DeleteByID(ec echo.Context) error {
@@ -23,8 +24,19 @@ func (c Controllers) DeleteByID(ec echo.Context) error {
 	loginData := ec.Get("x-user").(token.Data)
 
 	if err := c.Usecases.Farms.DeleteByID(ec.Request().Context(), id, loginData); err != nil {
-		return generateResp.NewJSONResponse(ec, http.StatusBadRequest, err.Error(), err)
+		return c.buildErrorDeleteByID(ec, err)
 	}
 
 	return generateResp.NewJSONResponse(ec, http.StatusOK, "Success", nil)
+}
+
+func (c Controllers) buildErrorDeleteByID(ec echo.Context, err error) error {
+	statusCode := http.StatusInternalServerError
+
+	switch err {
+	case gorm.ErrRecordNotFound:
+		statusCode = http.StatusNotFound
+	}
+
+	return generateResp.NewJSONResponse(ec, statusCode, err.Error(), err)
 }
